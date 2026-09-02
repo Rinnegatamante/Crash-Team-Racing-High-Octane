d1d6a4dd620cb7220a04bd112534401486c7bcf7
Rinnegatamante <rinnegatamante@gmail.com>
2026-09-03 01:56:02 +0200

    Fix several Cup mode animations being sped up at 60 FPS.

--- game/UI/UI_CupStandings.c
+++ game/UI/UI_CupStandings.c
@@ -209,18 +209,18 @@ void UI_CupStandings_InputAndDraw(void)
 	}
 
 	// Conditions to increment frame counter
-	if ((sdata->framesSinceRaceEnded < UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME) ||
-	    ((sdata->framesSinceRaceEnded < UI_CUP_STANDINGS_OVERALL_HOLD_FRAME) && ((sdata->menuReadyToPass & UI_CUP_STANDINGS_PAGE_OVERALL_POINTS) != 0)))
+	if ((sdata->framesSinceRaceEnded < FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME)) ||
+	    ((sdata->framesSinceRaceEnded < FPS_DOUBLE(UI_CUP_STANDINGS_OVERALL_HOLD_FRAME)) && ((sdata->menuReadyToPass & UI_CUP_STANDINGS_PAGE_OVERALL_POINTS) != 0)))
 	{
 		sdata->framesSinceRaceEnded++;
 	}
 
-	if ((sdata->framesSinceRaceEnded < UI_CUP_STANDINGS_QUICK_SKIP_FRAME) &&
+	if ((sdata->framesSinceRaceEnded < FPS_DOUBLE(UI_CUP_STANDINGS_QUICK_SKIP_FRAME)) &&
 
 	    // If you press Cross or Circle
 	    ((sdata->AnyPlayerTap & UI_CUP_STANDINGS_CONFIRM_BUTTONS) != 0))
 	{
-		sdata->framesSinceRaceEnded = UI_CUP_STANDINGS_QUICK_SKIP_FRAME;
+		sdata->framesSinceRaceEnded = FPS_DOUBLE(UI_CUP_STANDINGS_QUICK_SKIP_FRAME);
 		sdata->numIconsEOR = numDrivers;
 		RECTMENU_ClearInput();
 	}
@@ -242,7 +242,7 @@ void UI_CupStandings_InputAndDraw(void)
 	int titleEndX;
 	int titleEndY;
 
-	if (sdata->framesSinceRaceEnded <= UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME)
+	if (sdata->framesSinceRaceEnded <= FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME))
 	{
 		titleStartX = UI_CUP_STANDINGS_TITLE_START_X;
 		titleStartY = pageEnterY;
@@ -257,10 +257,10 @@ void UI_CupStandings_InputAndDraw(void)
 		titleStartY = UI_CUP_STANDINGS_TITLE_Y;
 		titleEndX = UI_CUP_STANDINGS_TITLE_END_X;
 		titleEndY = pageExitY;
-		animationFrame = sdata->framesSinceRaceEnded - UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME;
+		animationFrame = sdata->framesSinceRaceEnded - FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME);
 	}
 
-	UI_Lerp2D_Linear(drawPos.v, titleStartX, titleStartY, titleEndX, titleEndY, animationFrame, UI_CUP_STANDINGS_LERP_FRAMES);
+	UI_Lerp2D_Linear(drawPos.v, titleStartX, titleStartY, titleEndX, titleEndY, animationFrame, FPS_DOUBLE(UI_CUP_STANDINGS_LERP_FRAMES));
 
 	// "FINAL"
 	int titleString = LNG_FINAL;
@@ -306,7 +306,7 @@ void UI_CupStandings_InputAndDraw(void)
 
 	DecalFont_DrawLine(text, drawPos.x, drawPos.y + UI_CUP_STANDINGS_TRACK_TEXT_Y_OFFSET, FONT_SMALL, JUSTIFY_CENTER | ORANGE);
 
-	if ((sdata->framesSinceRaceEnded == (sdata->framesSinceRaceEnded / UI_CUP_STANDINGS_ICON_REVEAL_STEP_FRAMES) * UI_CUP_STANDINGS_ICON_REVEAL_STEP_FRAMES) &&
+	if ((sdata->framesSinceRaceEnded == (sdata->framesSinceRaceEnded / FPS_DOUBLE(UI_CUP_STANDINGS_ICON_REVEAL_STEP_FRAMES)) * FPS_DOUBLE(UI_CUP_STANDINGS_ICON_REVEAL_STEP_FRAMES)) &&
 	    (sdata->numIconsEOR < numDrivers))
 	{
 		sdata->numIconsEOR++;
@@ -392,22 +392,22 @@ void UI_CupStandings_InputAndDraw(void)
 
 	HaveIconTargetPos:
 
-		if (sdata->framesSinceRaceEnded <= UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME)
+		if (sdata->framesSinceRaceEnded <= FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME))
 		{
 			iconStartX = UI_CUP_STANDINGS_TITLE_END_X;
 			iconEndX = (int)iconTargetX;
-			animationFrame = sdata->framesSinceRaceEnded - (i * UI_CUP_STANDINGS_ICON_REVEAL_STEP_FRAMES);
+			animationFrame = sdata->framesSinceRaceEnded - (i * FPS_DOUBLE(UI_CUP_STANDINGS_ICON_REVEAL_STEP_FRAMES));
 		}
 
 		else
 		{
 			iconStartX = (int)iconTargetX;
 			iconEndX = UI_CUP_STANDINGS_TITLE_START_X;
-			animationFrame = sdata->framesSinceRaceEnded + -UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME;
+			animationFrame = sdata->framesSinceRaceEnded - FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME);
 		}
 
 		// Interpolate fly-in variables over 0x14 frames
-		UI_Lerp2D_Linear(drawPos.v, iconStartX, iconTargetY, iconEndX, iconTargetY, animationFrame, UI_CUP_STANDINGS_LERP_FRAMES);
+		UI_Lerp2D_Linear(drawPos.v, iconStartX, iconTargetY, iconEndX, iconTargetY, animationFrame, FPS_DOUBLE(UI_CUP_STANDINGS_LERP_FRAMES));
 
 		// %d
 		sprintf(text, (char *)&sdata->s_int, i + 1);
@@ -481,13 +481,13 @@ void UI_CupStandings_InputAndDraw(void)
 	{
 		// fly-in interpolation
 		UI_Lerp2D_Linear(drawPos.v, UI_CUP_STANDINGS_PANEL_X, pageEnterY, UI_CUP_STANDINGS_PANEL_X, UI_CUP_STANDINGS_PANEL_Y, sdata->framesSinceRaceEnded,
-		                 UI_CUP_STANDINGS_LERP_FRAMES);
+		                 FPS_DOUBLE(UI_CUP_STANDINGS_LERP_FRAMES));
 	}
 
 	// if it's not...
 	else
 	{
-		if (sdata->framesSinceRaceEnded <= UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME)
+		if (sdata->framesSinceRaceEnded <= FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME))
 		{
 			drawPos.x = UI_CUP_STANDINGS_PANEL_X;
 			drawPos.y = UI_CUP_STANDINGS_PANEL_Y;
@@ -497,7 +497,7 @@ void UI_CupStandings_InputAndDraw(void)
 		{
 			// fly-in interpolation
 			UI_Lerp2D_Linear(drawPos.v, UI_CUP_STANDINGS_PANEL_X, UI_CUP_STANDINGS_PANEL_Y, UI_CUP_STANDINGS_PANEL_X, pageExitY,
-			                 sdata->framesSinceRaceEnded + -UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME, UI_CUP_STANDINGS_LERP_FRAMES);
+			                 sdata->framesSinceRaceEnded - FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME), FPS_DOUBLE(UI_CUP_STANDINGS_LERP_FRAMES));
 		}
 	}
 
@@ -511,11 +511,11 @@ void UI_CupStandings_InputAndDraw(void)
 	RECTMENU_DrawInnerRect(&r, 4, gGT->backBuffer->otMem.uiOT);
 
 	// Timer
-	if (sdata->framesSinceRaceEnded < UI_CUP_STANDINGS_DONE_FRAME)
+	if (sdata->framesSinceRaceEnded < FPS_DOUBLE(UI_CUP_STANDINGS_DONE_FRAME))
 	{
 		if ((
 		        // Timer
-		        (UI_CUP_STANDINGS_PRESS_PROMPT_FRAME < sdata->framesSinceRaceEnded) &&
+		        (FPS_DOUBLE(UI_CUP_STANDINGS_PRESS_PROMPT_FRAME) < sdata->framesSinceRaceEnded) &&
 
 		        // If you are not in overall Cup standings
 		        ((sdata->menuReadyToPass & UI_CUP_STANDINGS_PAGE_OVERALL_POINTS) == 0)) &&
@@ -526,7 +526,7 @@ void UI_CupStandings_InputAndDraw(void)
 		     (sdata->AnyPlayerTap & UI_CUP_STANDINGS_CONFIRM_BUTTONS) != 0))
 		{
 			// Timer
-			sdata->framesSinceRaceEnded = UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME;
+			sdata->framesSinceRaceEnded = FPS_DOUBLE(UI_CUP_STANDINGS_PAGE_TRANSITION_FRAME);
 
 			// Proceed from Track standings to overall Cup standings,
 			// where you see how many points each driver has overall
